15c9438d20885d7602036a2c06c7f8ff4c72f290
Eugeny Konstantinov <jskonst@yandex.ru>
2021-01-20 19:11:26 +0300

    Add initial app on pygame

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 import pygame
 
-print(dir(pygame))
+pygame.init()
+screen = pygame.display.set_mode((640,480))
+background = pygame.Surface(screen.get_size())
+background.fill((255, 200, 200)) # R G B
+pygame.display.set_caption("VirusTracking")
+screen.blit(background,(0,0))
+
+mainloop = True
+
+while mainloop:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            print("QUIT")
+            mainloop = False
+    pygame.display.flip()
+
+pygame.quit()
